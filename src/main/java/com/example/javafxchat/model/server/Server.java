74d4c93972b333808d9ccd5a076e144faec2db34
tin <minhtien0601200@gmail.com>
2024-05-10 15:10:48 +0700

    add removeFromList and SendNofication method

--- src/main/java/com/example/javafxchat/model/server/Server.java
+++ src/main/java/com/example/javafxchat/model/server/Server.java
@@ -1,7 +1,9 @@
 package com.example.javafxchat.model.server;
 
+import com.example.javafxchat.model.exception.DuplicateUsernameException;
 import com.example.javafxchat.model.message.Message;
 import com.example.javafxchat.model.message.MessageType;
+import com.example.javafxchat.model.message.Status;
 import com.example.javafxchat.model.message.User;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -67,7 +69,45 @@ public class Server {
             return msg;
         }
 
-        private synchronized void checkDuplicateUsername(Message firstMessage) throws
+        private synchronized void checkDuplicateUsername(Message firstMessage) throws DuplicateUsernameException{
+            logger.info(firstMessage.getName() + " is trying to connect");
+            if(!names.containsKey(firstMessage.getName())){
+                this.name = firstMessage.getName();
+                user = new User();
+                user.setName(firstMessage.getName());
+                user.setStatus(Status.ONLINE);
+                user.setPicture(firstMessage.getPicture());
+
+                users.add(user);
+                names.put(name, user);
+
+                logger.info(name + " has been added to the list");
+            }else{
+                logger.error(firstMessage.getName() + " is already connected");
+                throw new DuplicateUsernameException(firstMessage.getName() + " is already connected");
+            }
+        }
+        private Message sendNotification(Message firstMessage) throws IOException{
+            Message msg = new Message();
+            msg.setMsg("has joined the chat.");
+            msg.setType(MessageType.NOTIFICATION);
+            msg.setName(firstMessage.getName());
+            msg.setPicture(firstMessage.getPicture());
+            write(msg);
+            return msg;
+        }
+
+        private Message removeFromList() throws IOException{
+            logger.debug("removeFromList() method Enter");
+            Message msg = new Message();
+            msg.setMsg(" has left the chat.");
+            msg.setType(MessageType.DISCONNECTED);
+            msg.setName("SERVER");
+            msg.setUserList(names);
+            write(msg);
+            logger.debug("removeFromList() method Exit");
+            return msg;
+        }
     }
 
 
